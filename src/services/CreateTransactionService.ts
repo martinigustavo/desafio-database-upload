e0ea95fb940ad83d4d46741485a31885fa3b76b3
Gustavo M <gustavomartini@protonmail.com>
2021-01-09 22:19:07 -0300

    added transactions creation' business rules

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,53 @@
+import { getCustomRepository } from 'typeorm';
+
 // import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CategoriesRepository from '../repositories/CategoriesRepository';
+import CreateCategoryService from './CreateCategoryService';
 
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getCustomRepository(CategoriesRepository);
+
+    const categoryExists = await categoriesRepository.findCategory(category);
+
+    let category_id;
+
+    if (!categoryExists) {
+      const newCategory = await new CreateCategoryService().execute({
+        title: category,
+      });
+
+      category_id = newCategory.id;
+    } else {
+      category_id = categoryExists;
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
